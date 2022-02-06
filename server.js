4bfe4f4024adb3988faf7bb593ff57afcb3f0003
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-02-06 17:26:26 +0200

    change pg

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 const pg = require("pg");
 
-dotenv.config();
 const DATABASE_URL = process.env.DATABASE_URL;
+
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
+dotenv.config();
 console.log(DATABASE_URL);
 
 const APIKEY = process.env.APIKEY;
